e4dc2cecad8f3545b5993b667686bf552791b078
Luke Brunning <la.brunning+github@gmail.com>
2015-08-07 11:11:17 +0100

    Removes SPAssignment method and adds Dispose method instead

--- Undeclare.ps1
+++ Undeclare.ps1
@@ -1,14 +1,13 @@
-$SPAssignment = Start-SPAssignment
-$web = Get-SPWeb https://devunishare.hud.ac.uk/unifunctions/committees/University-Committees -AssignmentCollection $spAssignment
+$web = Get-SPWeb https://devunishare.hud.ac.uk/unifunctions/committees/University-Committees
 $list = $web.lists["University Health and Safety Committee"].items
-foreach ($item in $list)
-{
+foreach ($item in $list) {
 	$IsRecord = [Microsoft.Office.RecordsManagement.RecordsRepository.Records]::IsRecord($Item)
-	if ($IsRecord -eq $true){
+	if ($IsRecord -eq $true) {
 		Write-Host "Undeclared $($item.Title)"
 		[Microsoft.Office.RecordsManagement.RecordsRepository.Records]::UndeclareItemAsRecord($Item)
-	}
-    $list.AllowDeletion = $true
-    $list.Update()
+    }
 }
-Stop-SPAssignment $SPAssignment
+$doclist = $web.lists["University Health and Safety Committee"]
+$doclist.AllowDeletion = $true
+$doclist.Update()
+$web.Dispose()
